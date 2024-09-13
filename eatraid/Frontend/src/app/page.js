fbc86bdbbfac686d17e866a8f7fbd1bc4380f8a4
thitirut111 <s6501012610033@kmutnb.ac.th>
2024-09-13 22:16:24 +0700

    เพิ่ม supabase

--- eatraid/Frontend/src/app/page.js
+++ eatraid/Frontend/src/app/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import supabase from './config/supabaseClient.js';
+
 export default function Home() {
   return (
     <div className={styles.page}>
