f33eb9b98d2233e09fb775d559e184e1437bb486
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-09-16 21:50:39 +0700

    Update Global Css
    - ใส่ฟร้อน poppins

--- eatraid/Frontend/src/app/page.js
+++ eatraid/Frontend/src/app/page.js
@@ -1,11 +1,12 @@
 import styles from "./page.module.css";
-
+import Navbar from "../../components/Navbar";
 import supabase from './config/supabaseClient.js';
 
 export default function Home() {
   console.log(supabase);
   return (
     <div className={styles.page}>
+      <Navbar></Navbar>
       EatRaiD
       by Barley
       mairuuka
