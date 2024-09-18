760e8edeef607772e7aa86703c09eb2a86361b59
thitirut111 <s6501012610033@kmutnb.ac.th>
2024-09-18 19:34:55 +0700

    แก้ไขให้สามารถส่ง OTP ไปยัง email ได้

--- eatraid/Backend/server.js
+++ eatraid/Backend/server.js
@@ -13,11 +13,22 @@ const supabaseUrl = 'https://gemuxctpjqhmwbtxrpul.supabase.co'
 const supabaseKey = process.env.ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+app.post("/signup-as-customer", async (req, res) => {
+  const { email, password } = req.body;
+  let { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: password
+  })
+  if (error) {
+      res.status(500).json(error);
+  }
+  else {
+      res.status(200).json(data);
+  }
+});
 
-<<<<<<< HEAD
 app.post("/verify-OTP", async (req, res) => {
     const { email,OTP } = req.body;
-    console.log('Request body:', req.body);
 
     const { data: { session }, error } = await supabase.auth.verifyOtp({
         email: email,
@@ -26,71 +37,28 @@ app.post("/verify-OTP", async (req, res) => {
       });
       
       if (error) {
-        console.error('Error:', error);
-      } else {
-        console.log('User is now signed up!', session);
-      }
-      
-});
-
-app.post("/login", async (req, res) => {
-    const { email,password } = req.body;
-    console.log('Request body:', req.body);
-
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-    });
-    
-    if (error) {
-        return res.status(401).json({ message: 'Login failed', error: error.message });
-    } else {
-        return res.status(200).json({ message: 'Login successful', user: data.user, session: data.session });
-=======
-//signup customer
-app.post("/signup-as-customer", async (req, res) => {
-    const { email, password } = req.body;
-    const {error} = await supabase.auth.signInWithOtp({
-        email: email,
-        password: password,
-        options: {
-            shouldCreateUser: false,
-        },
-    });
-    if (error) {
-        res.status(500).json(error);
-    }
-    else {
-        const { data, error } = await supabase.from('User').insert([{ Email: email, Password: password, Role: "customer" }]).select("*");
-        if (error) {
-            res.status(400).json(error);
-        }
-        else {
-            res.status(200).json(data);
-        }
->>>>>>> signup-user/backend
-    }
-});
-
-
-
-
-<<<<<<< HEAD
-=======
-app.delete("/delete-user", async (req, res) => {
-    const { data, error } = await supabase.auth.admin.deleteUser(
-        '6761abe7-7ca1-4406-80ba-18ac1288a871'
-    )
-    if (error) {
         res.status(400).json(error);
-    }
-    else {
-        res.status(200).json(data);
-    }
+      } else {
+        const { data, error } = await supabase.from('User').insert([{ Id: session.id, Role: "customer" }]).select("*");
+          if (error) {
+              res.status(400).json(error);
+          }
+          else {
+            console.log(session.id)
+            res.status(200).json({"verify email": session, "insert data to table user": data})
+          }}
 });
 
+app.delete("/delete-user", async (req, res) => {
+  const { data, error } = await supabase.auth.admin.deleteUser(
+      '723b7ddf-38c9-4eac-8a2e-07f87e09e418'
+  )
+  if (error) {
+    res.status(400).json(error);
+  }
+  else {
+      res.status(200).json(data);
+  }
+});
 
-
-
->>>>>>> signup-user/backend
 app.listen(port, () => console.log(`Server is running on port ${port}`));
