6bd4a21b0f9a12e78c715008228a3feb8fb9ec84
thitirut111 <s6501012610033@kmutnb.ac.th>
2024-09-19 22:40:01 +0700

    เพิ่ม api ที่ใช้ในการลบร้านออกจากรายการที่ชอบ

--- eatraid/Backend/server.js
+++ eatraid/Backend/server.js
@@ -49,6 +49,23 @@ app.post("/add-to-fav", async (req, res) => {
     }
 });
 
+app.delete("/delete-fav", async (req, res) => {
+  const { user, restaurant } = req.body;
+  
+  const { error } = await supabase
+  .from('Favorite')
+  .delete()
+  .eq('UserId', user)
+  .eq('RestaurantId', restaurant);
+        
+  if (error) {
+    res.status(400).json(error);
+  }
+  else {
+      res.status(200).json({'msg': "delete user's fav restaurant successfully"});
+  }
+});
+
 // ===========================test===========================
 
 app.delete("/delete-user", async (req, res) => {
