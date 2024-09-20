30022086e4e44d0e683cb4b8edb354b2b187194b
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-09-20 14:11:28 +0700

    ปรับให้อยู่ตรงกลาง

--- eatraid/Frontend/src/app/login/page.jsx
+++ eatraid/Frontend/src/app/login/page.jsx
@@ -9,49 +9,51 @@ export default function login() {
     return (
         <div className={styles.main}>
             <Topbar></Topbar>
-            <div className={styles.container}>
-                <div className={styles.From_Login}>
-                    <div className={styles.From_Login_header}>
-                        Log in
-                    </div>
-                    <div className={styles.From_Login_input}>
-                        <div className={styles.From_Login_input_Email}>
-                            <input className={styles.Loginblock}
-                                placeholder="Email"
-                                type="text"
-                                name="username"
-                                // value={formData.username}
-                                // onChange={handleInputChange}
-                                required
-                            />
-                        </div>
-                        <div className={styles.From_Login_input_Password}>
-                            <input className={styles.Loginblock}
-                                placeholder="Password"
-                                type="password"
-                                name="password"
-                                // value={formData.password}
-                                // onChange={handleInputChange}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className={styles.Loginbtn_wrapper}>
-                        <button className={styles.Loginbtn}>
+            <div className={styles.content_wrapper}>
+                <div className={styles.container}>
+                    <div className={styles.From_Login}>
+                        <div className={styles.From_Login_header}>
                             Log in
-                        </button>
+                        </div>
+                        <div className={styles.From_Login_input}>
+                            <div className={styles.From_Login_input_Email}>
+                                <input className={styles.Loginblock}
+                                    placeholder="Email"
+                                    type="text"
+                                    name="username"
+                                    // value={formData.username}
+                                    // onChange={handleInputChange}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.From_Login_input_Password}>
+                                <input className={styles.Loginblock}
+                                    placeholder="Password"
+                                    type="password"
+                                    name="password"
+                                    // value={formData.password}
+                                    // onChange={handleInputChange}
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.Loginbtn_wrapper}>
+                            <button className={styles.Loginbtn}>
+                                Log in
+                            </button>
+                        </div>
+                        <div className={styles.last_Login_container}>
+                            <p>
+                                Don't have an account?
+                            </p>
+                            <Link href={`/signup`}>
+                                Sign up
+                            </Link>
+                        </div>
                     </div>
-                    <div className={styles.last_Login_container}>
-                        <p>
-                            Don't have an account?
-                        </p>
-                        <Link href={`/signup`}>
-                            Sign up
-                        </Link>
+                    <div className={styles.Login_Picture}>
+                        <Image></Image>
                     </div>
-                </div>
-                <div className={styles.Login_Picture}>
-                    <Image></Image>
                 </div>
             </div>
         </div>
