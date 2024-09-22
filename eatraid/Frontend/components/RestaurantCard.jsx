9979986ced703b2dec4980dbd80e3eb09355a1b1
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-09-22 18:13:28 +0700

    ทำ Pagination เพิ่มเข้ามา

--- eatraid/Frontend/components/RestaurantCard.jsx
+++ eatraid/Frontend/components/RestaurantCard.jsx
@@ -1,22 +1,23 @@
 'use client';
 import styles from './RestaurantCard.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
+import { BsHeartFill,BsHeart } from "react-icons/bs";
 
 
 const RestaurantCard = (props) => {
-    const { img, title, name } = props;
+    const { img, name, id } = props;
     return (
-        <Link href={`/ProfileRestaurant/${name}`} className={styles.link_blog}>
+        <Link href={`/ProfileRestaurant/${id}`} className={styles.link_blog}>
             <div className={styles.content}>
                 <div className={styles.main_content}>
-
                     <div className={styles.singleDest}>
                         <div className={styles.dastImage}>
-                            <img src={img} alt="" className={styles.Imagecover} />
+                            <Image src={img} alt={`Restaurant ${name}`} className={styles.Imagecover} />
                         </div>
                         <div className={styles.destFooter}>
                             <div className={styles.destText}>
-                                {title}
+                                {name}
                             </div>
                         </div>
                     </div>
