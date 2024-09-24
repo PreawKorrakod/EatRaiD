3790902c5f5db50192dce7def2f4fa2ef6c0f6ae
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-09-25 02:17:15 +0700

    ๊edit Upload img

--- eatraid/Frontend/components/MenuCard.jsx
+++ eatraid/Frontend/components/MenuCard.jsx
@@ -3,9 +3,126 @@ import { useState } from 'react';
 import styles from './MenuCard.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
-import { BsPencilSquare, BsXSquareFill, BsFillTrashFill } from "react-icons/bs";
+import { BsPencilSquare, BsXSquareFill, BsFillTrashFill, BsCheckLg } from "react-icons/bs";
+
+const categoryDropdown = ["Thai", "Japanese"];
 
 const MenuCard = (props) => {
+
+    const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false); // State สำหรับสถานะการรอ
+    const [isSuccess, setIsSuccess] = useState(false); // State สำหรับการลบสำเร็จ
+    const [profileImage, setProfileImage] = useState('');
+    const [Imagefile, setImagefile] = useState('');
+
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setProfileImage(reader.result);
+            };
+            reader.readAsDataURL(file);
+            setImagefile(file);
+        }
+    };
+
+
+    // ฟังก์ชันลบ  backend ตรงนี้นะ
+    const handleRemove = async () => {
+        setIsLoading(true); // เริ่มโหลด เป็นการตั้งสถานะโหลดของ frontend 
+        try {
+            // เวลา 2 วินาทีจำลองการลบ ตรงนี้เอาออกได้เลย สามารถเขียนโค้ด Backend ลบข้อมูลตรงนี้ได้
+            await new Promise((resolve) =>
+                setTimeout(resolve, 2000));
+            // เป็นการจำลอง
+
+            // อันนี้จำเป็นต้องวางไว้หลังจากลบข้อมูล โค้ดของ Backend ถ้าเกิดลบสำเร็จ
+            setIsLoading(false); // หยุดโหลด
+            setIsSuccess(true); // ลบสำเร็จ
+            setTimeout(() => {
+                setIsAlertModalOpen(false);
+                setIsSuccess(false); // รีเซ็ตสถานะการลบสำเร็จ
+            }, 2000); // ซ่อน modal หลังจากแสดงข้อความสำเร็จ 2 วินาที
+
+        } catch (error) {
+            setIsLoading(false); // หยุดโหลดถ้าเกิดข้อผิดพลาด
+            alert("Error occurred while removing"); // แสดงข้อผิดพลาดถ้ามี
+        }
+    };
+
+    // Modal delete
+    const renderAlertModal = () => {
+        return (
+            <div id="logoutModal" className={styles.modal}>
+                <form className={styles.modal_content}>
+                    <div className={styles.container}>
+                        <BsXSquareFill className={styles.close} onClick={() => setIsAlertModalOpen(false)} />
+                        <h2>Edit Menu</h2>
+                        {isLoading ? (
+                            <p>Please wait...</p>
+                        ) : isSuccess ? (
+                            <p className={styles.SuccessfulText}>Successfully delete!</p>
+                        ) : (
+                            <form className={styles.EditContent}>
+                                <div className={styles.profiledisplay}><img className={styles.pfp} alt="Profile" src={profileImage} width={265} height={265} /></div>
+                                <label className={styles.uploadbtn}>
+                                    <input
+                                        type="file"
+                                        accept="image/*"
+                                        hidden
+                                        onChange={handleFileChange}
+                                    />
+                                    <div>Upload Your Photo</div>
+                                </label>
+                                <div className={styles.inputNamefood}>
+                                    Name : <input type="text" placeholder="Your text here" />
+                                </div>
+                                <div className={styles.inputNamefood}>
+                                    Type :  <select
+                                        className={styles.optionTextStyles}
+
+                                    >
+                                        {categoryDropdown.map((category, index) => (
+                                            <option key={index} value={category}>
+                                                {category}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className={styles.inputPricefood}>
+                                    Price : <input type="text" placeholder="Your text here" /> ฿
+                                </div>
+                            </form>
+                        )}
+                        <div className={styles.clearfix}>
+                            {!isSuccess && (
+                                <>
+                                    <button
+                                        type="button"
+                                        className={styles.cancelbtn}
+                                        disabled={isLoading}
+                                    >
+                                        <BsCheckLg className={styles.Checkicon} />
+                                        Confirm
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className={styles.Removebtn}
+                                        onClick={handleRemove}
+                                        disabled={isLoading}
+                                    >
+                                        {isLoading ? "Removing..." : <><BsFillTrashFill className={styles.Deleteicon} /> Delete</>}
+                                    </button>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    };
 
     const { img, name, type, price } = props;
 
@@ -30,7 +147,9 @@ const MenuCard = (props) => {
                                 </div>
                             </div>
                             <div className={styles.menu_buttom}>
-                                <button className={styles.Editfood}>
+                                <button
+                                    className={styles.Editfood}
+                                    onClick={() => setIsAlertModalOpen(true)} >
                                     <BsPencilSquare className={styles.Editicon} />
                                     Edit
                                 </button>
@@ -39,6 +158,7 @@ const MenuCard = (props) => {
                     </div>
                 </div>
             </div >
+            {isAlertModalOpen && renderAlertModal()}
         </>
     );
 };
