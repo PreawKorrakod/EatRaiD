d6f6aa8a180de5a9a86255d83077eea6e6bba15c
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-10-05 15:20:43 +0700

    Update page.jsx

--- eatraid/Frontend/src/app/page.jsx
+++ eatraid/Frontend/src/app/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 import styles from "./page.module.css";
 import Navbar from "../../components/Navbar";
+import { IoSearch } from "react-icons/io5";
 import { NEXT_PUBLIC_BASE_API_URL } from "./config/supabaseClient";
 
 export default function Home() {
@@ -11,7 +12,18 @@ export default function Home() {
       <div className={styles.Container}>
         <div className={styles.Search_Filter_wrapper}>
           <div className={styles.search_wrapper}>
-
+            <form className={styles.SearchBox}>
+              <div className={styles.Search_inside}>
+                <IoSearch size={25} className={styles.icon_Search} type='submit' />
+                <input
+                  type="text"
+                  // value={searchQuery}
+                  placeholder="Search here..."
+                  className={styles.inputSearch}
+                />
+              </div>
+              <button type='submit' className={styles.SearchBox_btn}>Search</button>
+            </form>
           </div>
           <div className={styles.Filter_wrapper}>
 
