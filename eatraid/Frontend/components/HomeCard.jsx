07e534c1de1d9963099491c77b448bdf5529f2e6
Korrakod Poonpradit <s6501012610017@email.kmutnb.ac.th>
2024-10-06 21:42:04 +0700

    ทำที่โชว์ร้านอาหาร

--- eatraid/Frontend/components/HomeCard.jsx
+++ eatraid/Frontend/components/HomeCard.jsx
@@ -1,10 +1,29 @@
 'use client';
 import styles from './HomeCard.module.css'
+import Image from 'next/image';
 
 const HomeCard = (props) => {
-    return(
-        <div>
-            Card
+    const { id, img, name } = props;
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.main_content}>
+                <div className={styles.singleDest}>
+
+                    <div className={styles.dastImage}>
+                        <Image src={img} alt={`Restaurant ${name}`} className={styles.Imagecover} width={300} height={200} />
+                    </div>
+
+                    <div className={styles.dastFooter}>
+                        <div className={styles.link_blog2}>
+                            <div className={styles.destText}>
+                                <p>{name}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
--- eatraid/Frontend/src/app/page.js
+++ eatraid/Frontend/src/app/page.js
@@ -2,23 +2,47 @@
 import React, { useState } from "react";
 import styles from "./page.module.css";
 import Navbar from "../../components/Navbar";
+import HomeCard from "../../components/HomeCard";
 import { IoSearch } from "react-icons/io5";
 import image1 from "../../public/imgTest1.png";
 import image2 from "../../public/imgTest2.png";
 import image3 from "../../public/imgTest3.png";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi";
 
 const data = [
-  { id: 1, name: "food A", image: image1 },
-  { id: 2, name: "food B", image: image2 },
-  { id: 3, name: "food C", image: image3 },
-  { id: 4, name: "food A", image: image1 },
-  { id: 5, name: "food B", image: image2 },
+  { id: 1, name: "food A", image: image1, type: "noodle" },
+  { id: 2, name: "food B", image: image2, type: "fastfood" },
+  { id: 3, name: "food C", image: image3, type: "fastfood" },
+  { id: 4, name: "food A", image: image1, type: "order to cooked" },
+  { id: 5, name: "food B", image: image2, type: "noodle" },
 ];
 
 export default function Home() {
   const [isFocused, setIsFocused] = useState(false);
   const handleFocus = () => setIsFocused(true);
   const handleBlur = () => setIsFocused(false);
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
   return (
     <div className={styles.page}>
@@ -42,7 +66,7 @@ export default function Home() {
                 type="text"
                 placeholder="Search..."
                 className={styles.inputSearch}
-                onFocus={handleFocus} // จับเหตุการณ์ focus
+                onFocus={handleFocus}
                 onBlur={handleBlur}
               />
             </div>
@@ -54,13 +78,55 @@ export default function Home() {
             <div className={styles.Filter_header}>Filter Restaurant</div>
             <div className={styles.Filter_Contaniner}></div>
           </div>
-          
         </div>
         <div className={styles.List_Containner}>
           <div className={styles.random_wrapper}>
-            <randomCard/>
+            <div className={styles.slider}>
+              <Carousel responsive={responsive} style={{ zIndex: "900" }}>
+                {data.map((card) => {
+                  return (
+                    <div key={card.id}>
+                      <HomeCard
+                        img={card.image}
+                        name={card.name}
+                        type={card.type}
+                      />
+                    </div>
+                  );
+                })}
+              </Carousel>
+            </div>
+            <div className={styles.RandomContainer}>
+              <button className={styles.Randombtn}>
+                <span>
+                  <GiPerspectiveDiceSixFacesRandom
+                    size={25}
+                    className={styles.randomicon}
+                  />
+                  Random
+                </span>
+              </button>
+            </div>
+            <div className={styles.Res_wrapper}>
+              <div className={styles.AllmenuContaniner}>
+                <div className={styles.Allmenu_header}>
+                  <div className={styles.title}>
+                    All Restaurant
+                    <div className={styles.ShowSelect}>
+                      Select{" "}
+                      <div className={styles.Showlength}>{data.length}</div>
+                    </div>
+                  </div>
+                  <div className={styles.subtitle}>
+                    Uncheck to remove from random
+                  </div>
+                </div>
+                <div className={styles.listRes}>
+
+                </div>
+              </div>
+            </div>
           </div>
-          <div className={styles.List_wrapper}></div>
         </div>
       </div>
     </div>
